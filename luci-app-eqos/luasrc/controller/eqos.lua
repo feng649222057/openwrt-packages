056aca6c2d82d40fef586f8ea1ebc21fedee12fd
feng649222057 <wind649@vip.qq.com>
2023-02-26 21:52:15 +0800

    优化调整

--- luci-app-eqos/luasrc/controller/eqos.lua
+++ luci-app-eqos/luasrc/controller/eqos.lua
@@ -1,10 +1,11 @@
 module("luci.controller.eqos", package.seeall)
 
 function index()
-	if not nixio.fs.access("/etc/config/eqos") then
-		return
-	end
-
-	local page = entry({"admin", "network", "eqos"}, cbi("eqos"), "EQoS")
-	page.dependent = true
+    if not nixio.fs.access("/etc/config/eqos") then
+        return
+    end
+    local page
+    entry({ "admin", "control" }, firstchild(), "Control", 44).dependent = false
+    page = entry({ "admin", "control", "eqos" }, cbi("eqos"), "EQoS")
+    page.dependent = true
 end
