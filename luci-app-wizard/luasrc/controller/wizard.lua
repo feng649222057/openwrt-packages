e2faddc90b3a3c98db35d6136849289b7afbde8a
feng649222057 <wind649@vip.qq.com>
2023-02-26 09:38:39 +0800

    优化调整

--- luci-app-wizard/luasrc/controller/wizard.lua
+++ luci-app-wizard/luasrc/controller/wizard.lua
@@ -7,7 +7,7 @@ function index()
 		return
 	end
 		local page 
-		page = entry({"admin", "system", "wizard"}, cbi("wizard/wizard"), _("Inital Setup"), 0)
+		page = entry({"admin", "system", "wizard"}, cbi("wizard/wizard"), _("Inital Setup"), 1)
 		page.i18n = "wizard"
 		page.dependent = true
 
